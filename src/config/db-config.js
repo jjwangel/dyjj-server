c68df5e77d59e46a4fe85ca0092e69949ec64271
jjwangel <93108@qq.com>
2018-01-23 16:09:39 +0800

    init

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -1,32 +1,44 @@
-const dbconfig = {
-  conn1: {
-      user: 'sa',
-      password: 'sasa',
-      server: '127.0.0.1',
-      database: 'TEST',
-      options: {
-          // encrypt: true // Use this if you're on Windows Azure
-      },
-      pool: {
-          max: 10,
-          min: 1,
-          idleTimeoutMillis: 30000
-      }
+module.exports.dbConfig = {
+  uf_account: {
+    user: 'sa',
+    password: 'sasa',
+    server: '127.0.0.1',
+    database: 'TEST',
+    options: {
+      // encrypt: true // Use this if you're on Windows Azure
+    },
+    pool: {
+      max: 10,
+      min: 1,
+      idleTimeoutMillis: 30000
+    }
   },
-  conn2: {
-      user: 'sa',
-      password: 'sasa',
-      server: '127.0.0.1',
-      database: 'CHI',
-      options: {
-          // encrypt: true // Use this if you're on Windows Azure
-      },
-      pool: {
-          max: 10,
-          min: 1,
-          idleTimeoutMillis: 30000
-      }
-  }
+  uf_system: {
+    user: 'sa',
+    password: 'sasa',
+    server: '127.0.0.1',
+    database: 'CHI',
+    options: {
+      // encrypt: true // Use this if you're on Windows Azure
+    },
+    pool: {
+      max: 10,
+      min: 1,
+      idleTimeoutMillis: 30000
+    }
+  },
+  application: {
+    user: 'sa',
+    password: 'sasa',
+    server: '127.0.0.1',
+    database: 'CHI',
+    options: {
+      // encrypt: true // Use this if you're on Windows Azure
+    },
+    pool: {
+      max: 10,
+      min: 1,
+      idleTimeoutMillis: 30000
+    }
+  },
 };
-
-module.exports.dbConfig = dbconfig;
